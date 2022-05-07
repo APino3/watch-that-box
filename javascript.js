e8adb3c078221fdfa19268d56c33bb87ad74e78b
Aespino <albaliciae@gmail.com>
2022-05-07 13:30:07 -0500

    javascript

---
+++ javascript.js
@@ -0,0 +1,24 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "250px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.background ="blue";
+
+});
+    
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height="150px";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.background="orange";
+
+});
+
